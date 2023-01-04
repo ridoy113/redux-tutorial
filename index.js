d3e3ce32b59bb18fda309025ae9a7ab4503b9787
ridoy113 <almamounhridoy113@gmail.com>
2023-01-04 11:46:11 +0600

    create reducer

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 // defining constants
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
-const ADD_USER = "ADD_USER";
 
 // state
 const initialCounterState = {
   count: 0,
-};
-const initialUsersState = {
-  users: [{ name: "al mamun ridoy" }],
 };
 
 // action - object- type, payload
@@ -24,15 +20,28 @@ const decrementCounter = () => {
   };
 };
 
-const addUser = () => {
-  return {
-    type: ADD_USER,
-    payload: {name: 'kamal'}
-  };
+// create reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count:state.count + 1.
+      };
+   
+    case DECREMENT:
+        return {
+            ...state,
+            count:state.count - 1,
+          };
+   
+
+    default:
+      state;
+  }
 };
 
-
 // 1. state
-// 2. dispatch action 
-// 3. reducer 
+// 2. dispatch action
+// 3. reducer
 // 4. store
